7235f0cafdcddf2cc319c5b99f8e1b57533f4191
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-02-24 22:09:24 +0100

    Creating shop

--- unikaćWielokrotnie/index.js
+++ unikaćWielokrotnie/index.js
@@ -1,1 +1,24 @@
-$('#main-circle').on('click', () => console.log('clicked'))
+let biscuitsPerSec = 0;
+let biscuitsPerClick = 1;
+
+
+
+$('#main-circle').click(function () {
+    $('#cookie-counter').text( parseInt( $('#cookie-counter').text() ,10 ) + 1 );
+});
+
+const purchase = function(itemId) {
+    const item = $('#items li')[itemId];
+    let itemPrice = item.find('.item-price').text() - 0;
+    let itemCounter = item.find('.item-counter').text() - 0;
+    let cookieAmount = parseInt($('#cookie-counter').text(), 10);
+    cookieAmount -= price;
+    itemPrice++;
+    itemCounter++;
+    const itemType = item.dataset.itemType;
+    if(itemType == 'biscuitsPerSec')
+    {
+
+    }
+}
+
