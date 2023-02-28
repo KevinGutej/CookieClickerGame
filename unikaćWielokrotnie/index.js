a7d569fb8c7e74965163c0cc7d8b420ab7e561fb
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-02-28 22:37:26 +0100

    Testing around with js and local storage

--- unikaćWielokrotnie/index.js
+++ unikaćWielokrotnie/index.js
@@ -1,18 +1,31 @@
 let biscuitsPerSec = 0;
 let biscuitsPerClick = 1;
 
+$( document ).ready(function() {
+    let biscuitAmount = window.localStorage.getItem("biscuitAmount");
+    if(biscuitAmount == NaN || biscuitAmount == undefined || biscuitAmount == null)
+    {
+        biscuitAmount = 0;
+    }
+    else {
+        $('#biscuit-counter').text(biscuitAmount);
+    }
+    
+});
 
 
 $('#main-circle').click(function () {
-    $('#cookie-counter').text( parseInt( $('#cookie-counter').text() ,10 ) + 1 );
+    const biscuitAmount = parseInt( $('#biscuit-counter').text() ,10 ) + 1;
+    $('#biscuit-counter').text(biscuitAmount);
+    window.localStorage.setItem("biscuitAmount", biscuitAmount);
 });
 
 const purchase = function(itemId) {
     const item = $('#items li')[itemId];
     let itemPrice = item.find('.item-price').text() - 0;
     let itemCounter = item.find('.item-counter').text() - 0;
-    let cookieAmount = parseInt($('#cookie-counter').text(), 10);
-    cookieAmount -= price;
+    let biscuitAmount = parseInt($('#biscuit-counter').text(), 10);
+    biscuitAmount -= price;
     itemPrice++;
     itemCounter++;
     const itemType = item.dataset.itemType;
@@ -21,4 +34,7 @@ const purchase = function(itemId) {
 
     }
 }
+
+
+
 
